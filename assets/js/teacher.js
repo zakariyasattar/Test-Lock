39173f2bd033b713a10804aab5b3a2560e1d2d2c
Zakariya Sattar <zakariyasattar03@gmail.com>
2019-05-30 11:56:43 -0500

    completely style teacher page. LOOKING AWESOME!

--- assets/js/teacher.js
+++ assets/js/teacher.js
@@ -1,57 +1,76 @@
-// // check if user is signed in
-// if(localStorage.getItem('userInfo') == null) {
-//   document.getElementById('body').style.display = "none";
-//   alert("NOT AUTHORIZED");
-// }
-//
+// check if user is signed in
+if(localStorage.getItem('userInfo') == null) {
+  document.getElementById('body').style.display = "none";
+  alert("NOT AUTHORIZED");
+}
 
-// Pull all exam codes and seperate the datapoint from each other
+
 var examCodes = [];
 var examCodesTeachers = [];
 var arr = [];
 
+//when page loads populateDashboard()
 window.onload = function() {
   populateDashboard();
 };
 
-firebase.database().ref('exam-codes').on('value', function(snapshot) {
-  snapshot.forEach(function(childSnapshot) {
-    examCodes.push(childSnapshot.val().split(";")[0]);
-    examCodesTeachers.push(childSnapshot.val().split(";")[1])
-  });
-});
-
-
+// retrieve userName from localStorage
 var userName = (JSON.parse(localStorage.getItem("userInfo"))[1]);
+var profileImg = (JSON.parse(localStorage.getItem("userInfo"))[2]);
+
+//initialize aspects of page with JS data
+document.getElementById('welcome-text').innerHTML = "Welcome " + userName + "!";
+document.getElementById('profile').src = profileImg;
 
 function createQuiz() {
+  if(examCodes.length == 0){
+
+    // Pull all exam codes and seperate the datapoint from each other
+    firebase.database().ref('exam-codes').on('value', function(snapshot) {
+      snapshot.forEach(function(childSnapshot) {
+        examCodes.push(childSnapshot.val().split(";")[0]);
+        examCodesTeachers.push(childSnapshot.val().split(";")[1])
+      });
+    });
+  }
+
+  // get randomCode
   var randomCode = generateCode();
 
-  if(examCodes.indexOf(randomCode) == -1){
-    firebase.database().ref("exam-codes").push(randomCode.toUpperCase() + ";" + userName); //+ ";" + object.innerHTML);
-  }
-  else {
-    createQuiz();
-  }
 }
 
+// Generate random exam code
 function generateCode() {
-  return Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
+  var code = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5)
+  if(examCodes.indexOf(code) == -1){
+    firebase.database().ref("exam-codes").push(randomCode.toUpperCase() + ";" + userName); //+ ";" + object.innerHTML);
+    return code;
+  }
+  else {
+    generateCode();
+  }
 }
 
 // create div box for every class
-function createClassBox(className, size) {
+function createClassBox(className) {
   var wrapper = document.getElementById('wrapper');
-  wrapper.style = "grid-template-columns: repeat(" + size + ", 1fr)";
 
   var classBox = document.createElement('div');
-  classBox.innerHTML = className;
+  classBox.className = "classBox";
+
+  var span = document.createElement('span');
+  span.innerHTML = className;
+  span.className = 'className';
+
+  classBox.appendChild(span);
+  classBox.appendChild(document.createElement('hr'));
 
   wrapper.appendChild(classBox);
 
   document.body.appendChild(wrapper);
 }
 
+//populate main dashboard of page when page loads
 function populateDashboard() {
   var ref = firebase.database().ref('Teachers');
   var counter = 0;
@@ -67,16 +86,11 @@ function populateDashboard() {
           var obj = arr[key];
           for (var prop in obj) {
 
-            var size = 0, key;
-            for (key in obj) {
-                if (obj.hasOwnProperty(key)) size++;
-            }
-
               // skip loop if the property is from prototype
               if(!obj.hasOwnProperty(prop)) continue;
 
               // your code
-              createClassBox(prop, size);
+              createClassBox(prop);
           }
         }
       }
