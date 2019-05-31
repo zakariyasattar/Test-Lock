58db678bebc4beb4f1efeeb9dbf8ff95a334d33f
Zakariya Sattar <zakariyasattar03@gmail.com>
2019-05-30 23:02:53 -0500

    implement full screen and valid exam code

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,13 @@
+var examCodes = [];
+var examCodesTeacher = [];
+
+firebase.database().ref('exam-codes').on('value', function(snapshot) {
+  snapshot.forEach(function(childSnapshot) {
+    examCodes.push(childSnapshot.val().split(";")[0]);
+    examCodesTeacher.push(childSnapshot.val().split(";")[1]);
+  });
+});
+
 function onSignIn(googleUser) {
   if(document.URL.indexOf("?") == -1 && document.URL.substring(document.URL.indexOf('?') + 1) != "noRedirect"){
     window.location = "teacher.html";
@@ -18,11 +28,14 @@ function signOut() {
   });
 }
 
+
+//function to remove all class='active'
 function removeAllActive() {
   document.getElementsByTagName("LI")[0].className = "";
   document.getElementsByTagName("LI")[1].className = "";
 }
 
+//function to switch displays based on what user clicks
 function display(title) {
   if(title == '<a href="#">Ping-Pong</a>') {
     document.getElementById('main').style.display = "none";
@@ -34,14 +47,15 @@ function display(title) {
   }
 }
 
+// function to retieve and analyze code
 function submitExamCode() {
   var code = document.getElementById('inputExamCode').value.toUpperCase();
-  console.log(code);
 
   if(isValid(code)) {
     localStorage.setItem('ExamCode', code);
     document.getElementById('main').style.display = "none";
     document.getElementById('navigation').style.display = "none";
+    displayQuiz(code, examCodesTeacher[examCodes.indexOf(code)]);
   }
   else {
     if(code.length > 0) {
@@ -50,8 +64,67 @@ function submitExamCode() {
   }
 }
 
+//function to make sure exam code is valid
 function isValid(code) {
-  var examExists = true;
-
-  return examExists && code.length == 5 && (!/[~!#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/g.test(code));
+  return examCodes.indexOf(code) != -1 && code.length == 5 && (!/[~!#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/g.test(code));
 }
+
+// function to display quiz to student
+function displayQuiz(code, teacher) {
+  console.log(code, teacher);
+  toggleFullScreen();
+}
+
+//toggleFullScreen
+function toggleFullScreen() {
+  if (!document.fullscreenElement) {
+    document.documentElement.requestFullscreen();
+  }
+  else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    }
+  }
+}
+
+//code to call when document leaves full screen
+document.onfullscreenchange = function ( event ) {
+  if(document.fullscreenElement == null) {
+    var time = 5;
+    var x = setInterval(function(){
+        if(time != 0) {
+          time--;
+        }
+        else {
+          clearInterval(x);
+          endTest();
+        }
+     }, 1000);
+
+
+    swal({
+      title: "Are you sure?",
+      text: "Once you leave the test, you can no longer come back. Your final score will be recorded. YOU HAVE " + time + " SECONDS BEFORE THIS AUTOMATICALLY CLOSES AND ENDS TEST",
+      icon: "warning",
+      buttons: ["Take me back!", "Ok, end my test"],
+      dangerMode: true,
+    })
+    .then((willDelete) => {
+      if (willDelete) {
+        swal("Success! Your score has been recorded and sent to your teacher!", {
+          icon: "success",
+        });
+      } else {
+        toggleFullScreen();
+      }
+    });
+  }
+};
+
+
+// if user hits enter, check exam code
+document.addEventListener("keypress", function(e) {
+  if (e.keyCode === 13 && document.getElementById('main').style.display == "initial") {
+    submitExamCode();
+  }
+}, false);
