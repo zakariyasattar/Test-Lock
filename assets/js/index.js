e97a8e50b248129d0696516e065037439ab69d58
Zakariya Sattar <zakariyasattar03@gmail.com>
2019-06-06 22:43:16 -0500

    add real data!

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,25 +2,26 @@ var examCodes = [];
 var stopEnter = false;
 var key = initKey();
 
-// 
-// var data;
-// 	$.ajax({
-// 	  type: "GET",
-// 	  url: "data.csv",
-// 	  dataType: "text",
-// 	  success: function(response)
-// 	  {
-//   		data = $.csv.toArrays(response);
-//       for(var i = 0; i < data.length; i++){
-//         var id = data[i][0];
-//         var name = data[i][3] + " " + data[i][2];
-//         var className = data[i][10];
-//
-//         firebase.database().ref("Teachers/Zakariya Sattar/Classes/" + className).push(id + ";" + name);
-//         //firebase.database().ref("Teachers/Zakariya Sattar/Classes/" + className).remove();
-//       }
-// 	  }
-// });
+
+var data;
+	$.ajax({
+	  type: "GET",
+	  url: "data.csv",
+	  dataType: "text",
+	  success: function(response)
+	  {
+  		data = $.csv.toArrays(response);
+      for(var i = 0; i < data.length; i++){
+        var id = data[i][0];
+        var name = data[i][3] + " " + data[i][2];
+        var className = data[i][10];
+
+        //firebase.database().ref("Teachers/Zakariya Sattar/Classes/" + className + "/Students").push(id + ";" + name);
+        firebase.database().ref("Teachers/Zakariya Sattar/Classes/" + className + "/Exams").push("hello");
+        //firebase.database().ref("Teachers/Zakariya Sattar/Classes/" + className).remove();
+      }
+	  }
+});
 
 
 // pull all exam codes then encrypt them
