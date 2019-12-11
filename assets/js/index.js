435fd7507877ec868c2588b544fea64c0283d30d
Zakariya Sattar <zakariyasattar03@gmail.com>
2019-12-10 18:26:52 -0600

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -441,7 +441,7 @@ function retrieveName() {
         }
       });
 
-      if(!taken) {
+      if(!taken || taken) {
         document.getElementById('userName').style.color = "black";
         sessionStorage.setItem('idNum', id);
         firebase.database().ref("Teachers/" + plaintext.split(";")[1] + "/Classes/" + plaintext.split(";")[2] + "/Students").once('value', function(snapshot) {
